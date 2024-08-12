8d4c1925346f5023535a6114d76e6779ab715982
inyourtime <inyourtimeguy@gmail.com>
2024-08-12 10:01:56 +0700

    fix: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
-inject();
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
